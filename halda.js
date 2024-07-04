39d64941559a0073904c65cf3a3e580cd181a00c
aljuniedkhan <juniedk54@gmail.com>
2024-07-05 00:32:55 +0600

    4 Task done

---
+++ halda.js
@@ -0,0 +1,13 @@
+let x='10';
+let num = Number(x);
+console.log(num)
+
+let y='102abc';
+let num1 = Number(y);
+console.log(num1)
+
+const a = isNaN('9');
+console.log(a);
+
+const b = isNaN(4-'hello ');
+console.log(b);
---
+++ practiseTask.js
@@ -0,0 +1,21 @@
+// practise problem 1 task
+var iHave = 1000;
+var CostBazar = 700;
+var Moneyreturn = (iHave - CostBazar);
+console.log(Moneyreturn);
+
+// practise Problem 2 task
+var GotNum = (75.25+65+80+35.45+99.50);
+var avrNum =(GotNum/5);
+console.log(avrNum.toFixed(2));
+
+// practise problem 3 task
+var FirstNum = 119;
+var Ans = (119%5);
+console.log(Ans)
+
+// practise problem 4 task
+var a = isNaN('11');
+console.log(a);
+var b = isNaN(2-10);
+console.log(b);
